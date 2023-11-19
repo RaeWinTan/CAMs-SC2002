45d95413f70fc5bde11021669acbc4c521c3aac6
Robin DeBank <40622508+ngeeannrobin@users.noreply.github.com>
2023-11-19 20:24:18 +0800

    Implement Student make Enquiry

---
+++ src/main/java/com/example/datastore/operator/StudentAddEnquiry.java
@@ -0,0 +1,31 @@
+package com.example.datastore.operator;
+
+import java.util.ArrayList;
+
+import com.example.datastructure.Enquiry;
+import com.example.datastructure.Student;
+import com.example.exception.ObjectNotFoundException;
+
+public class StudentAddEnquiry implements IDataStoreEditOperation<Student> {
+
+    private Student student;
+    private Enquiry enquiry;
+
+    public StudentAddEnquiry(Student student, Enquiry enquiry){
+        this.student = student;
+        this.enquiry = enquiry;
+    }
+
+    @Override
+    public void perform(ArrayList<Student> data) {
+        // get student
+        for (Student student : data) {
+            if (student.isEquals(this.student)){
+                // add enquiry
+                student.getEnquireAbout().add(this.enquiry);
+                return;
+            }
+        }
+        throw new ObjectNotFoundException("Student");
+    }
+}
