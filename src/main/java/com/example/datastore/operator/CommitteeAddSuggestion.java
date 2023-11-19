60fd1b5d9a10f72d91fee46d73b10ef1899e0923
Robin DeBank <40622508+ngeeannrobin@users.noreply.github.com>
2023-11-19 20:38:47 +0800

    Implement committee make suggestion

---
+++ src/main/java/com/example/datastore/operator/CommitteeAddSuggestion.java
@@ -0,0 +1,32 @@
+package com.example.datastore.operator;
+
+import java.util.ArrayList;
+
+import com.example.datastructure.Student;
+import com.example.datastructure.Suggestion;
+import com.example.exception.ObjectNotFoundException;
+
+public class CommitteeAddSuggestion implements IDataStoreEditOperation<Student>{
+
+    private Student student;
+    private Suggestion suggestion;
+
+    public CommitteeAddSuggestion(Student student, Suggestion suggestion){
+        this.student = student;
+        this.suggestion = suggestion;
+    }
+
+    @Override
+    public void perform(ArrayList<Student> data) {
+        // Get student
+        for (Student student : data) {
+            if (student.isEquals(this.student)){
+                // Add suggestion to student
+                student.getSuggestions().add(this.suggestion);
+                return;
+            }
+        }
+        throw new ObjectNotFoundException("Student");
+    }
+    
+}
