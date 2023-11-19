fef3b18461628722cf13026df931f4bf1ac6a00b
Robin DeBank <40622508+ngeeannrobin@users.noreply.github.com>
2023-11-19 14:08:29 +0800

    Implement student registering as attendee

--- src/main/java/com/example/datastore/operator/StudentJoinCampAsAttendee.java
+++ src/main/java/com/example/datastore/operator/StudentJoinCampAsAttendee.java
@@ -2,6 +2,7 @@ package com.example.datastore.operator;
 
 import java.util.ArrayList;
 
+import com.example.datastore.DataStore;
 import com.example.datastructure.Camp;
 import com.example.datastructure.CampMember;
 import com.example.datastructure.Student;
@@ -11,21 +12,39 @@ public class StudentJoinCampAsAttendee implements DataStoreEditOperation<Camp> {
 
     Student student;
     Camp camp;
-    public StudentJoinCampAsAttendee(Student student, Camp camp){
+    DataStore<Student> studenDataStore;
+    public StudentJoinCampAsAttendee(Student student, Camp camp, DataStore<Student> studentDataStore){
         this.student = student;
         this.camp = camp;
+        this.studenDataStore = studentDataStore;
     }
 
     @Override
     public void perform(ArrayList<Camp> data) throws IllegalOperationException {
+        // Check if camp has enough slots
+        if (this.camp.getRemaindingSlots() <= 0){
+            throw new IllegalOperationException("Camp has reached the maximum participants.");
+        }
+
+        // TODO: Check for clash in dates
+
+        
         for (Camp camp : data) {
             if (camp.isEquals(this.camp)){
+                // Add student to attendee array in camp
                 camp.getAttendees().add(new CampMember(this.student, this.camp));
+
+                // Add camp to attending array in student
+                studenDataStore.manageData(new StudentAddAttendingCamp(this.student, this.camp));
                 return;
             }
         }
 
         throw new IllegalOperationException("Camp not found");
+
+        
+
+        
     }
     
 }
