8b12ae3e8dc13ca48070ce92c59d4c46c738aa7e
Robin DeBank <40622508+ngeeannrobin@users.noreply.github.com>
2023-11-23 20:28:25 +0800

    Add test code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import com.example.dataloader.StaffCSVLoader;
@@ -11,15 +12,12 @@ import com.example.datastore.operator.UserDataStoreLoad;
 import com.example.datastore.operator.UserLoginRetrival;
 import com.example.datastructure.Camp;
 import com.example.datastructure.Enquiry;
-
+import com.example.datastructure.GroupName;
 import com.example.datastructure.Staff;
 import com.example.datastructure.Student;
 import com.example.datastructure.Suggestion;
 import com.example.exception.InvalidLoginCredentialException;
-import com.example.view.AcceptRejectSuggestionPromptPage;
-import com.example.view.IPromptPage;
-import com.example.view.LoginPromptPage;
-import com.example.view.*;
+
 
 
 
@@ -42,96 +40,10 @@ public class App {
 
 	public static void main(String arg[]) {
 		initialise();
-		workFlow();
-	}
-	public static void login(){
-		String username = "";
-		String password = "";
-		String userType = "";
-		while (staff==null && student==null) {
-			IPromptPage loginPage = redirect("login");
-			loginPage.perform();
-			for(int i = 0; i < loginPage.returnInputs().size();i++){
-				if(loginPage.returnInputs().get(i).getResult().getFirst() == "username"){
-					username = loginPage.returnInputs().get(i).getResult().getSecond();
-				}
-				if(loginPage.returnInputs().get(i).getResult().getFirst() == "password"){
-					password = loginPage.returnInputs().get(i).getResult().getSecond();
-				}
-				if(loginPage.returnInputs().get(i).getResult().getFirst() == "userType"){
-					userType = loginPage.returnInputs().get(i).getResult().getSecond();
-				}
-			}
-			try {
-				if (userType.equals("Staff")){
-					staff = staffDataStore.retrieveData(new UserLoginRetrival<Staff>(username, password)).get(0);
-				} else{
-					student = studentDataStore.retrieveData(new UserLoginRetrival<Student>(username, password)).get(0);			
-				}
-			}
-			catch (InvalidLoginCredentialException e){
-				System.out.println(e.getMessage());
-			}
-		}
-	}
-	public static void workFlow(){
-		while(true){
-			login();
-			while(staff!=null||student!=null){//while it is logged in
-				IViewPage dashboard;
-				if(userType==UserType.STAFF){
-					dashboard = redirect("staffDashboard");
-				}else{
-					dashboard = redirect("studentDashboard");
-				}
-				dashboard.perform();
-				currentPage = ((IPromptPage) dashboard).returnInputs().get(0).getResult().getSecond();
-				if(currentPage.equals("logout")) break;
-				IViewPage newPage = redirect(currentPage);
-				if(newPage instanceof IPromptPage){
-						((IPromptPage)newPage).returnInputs();
-						//here must handel that 
-				}else{ //it is a display page
-
-				}
-			}
-			System.out.println("SUCCESSFUL LOGOUT");
-			staff = null;
-			student = null;
-			
-		}
-		
-
-		
+		Staff testStaff = loginStaff("UPAM", "password");
 	}
 
-	public static IViewPage redirect(String pagename){
-		//check for those page name that has an array set for them, got to create here
-		
-		currentPage = pagename;
-		//must do down casting either for display or prompt
-		if(pagename=="login"){
-			return new LoginPromptPage();
-		}else if(pagename == "studentDashboard"){
-			return new StudentDashboardPromptPage(isCommittee);
-		}
-		else if(pagename == "AcceptRejectSuggestionPromptPage"){
-			return new AcceptRejectSuggestionPromptPage();
-		}
-		else if(pagename == "withdrawFromCamp"){
-			return new CampWithdrawalPromptPage();
-		}
-		else if(pagename == "changePassword"){
-			return new ChangePasswordPromptPage();
-		}
-		else if(pagename == "CreateCampPromptPage"){
-			return new CreateCampPromptPage();
-		}
-		
-		else {
-		return new LoginPromptPage();}
-	} 
-
+	
 
 	private static void initialise(){
 		// Initialise Datastores.
@@ -144,9 +56,31 @@ public class App {
 		// Populate userDataStore with Staff and Student objects.
 		staffDataStore.manageData(new UserDataStoreLoad<Staff>(new StaffCSVLoader("./src/.data/staff.csv")));
 		studentDataStore.manageData(new UserDataStoreLoad<Student>(new StudentCSVLoader("./src/.data/student.csv")));
+	}
 
-		
-		
+	private static Staff loginStaff(String id, String pw){
+		return staffDataStore.retrieveData(new UserLoginRetrival<Staff>(id, pw)).get(0);
+	}
+
+	private static Student loginStudent(String id, String pw){
+		return studentDataStore.retrieveData(new UserLoginRetrival<Student>(id, pw)).get(0);
+	}
+
+	private static void campCreateTest(Staff staff){
+		Camp camp;
+		Date[] dates = {};
+		dates[0] = new Date();
+		dates[0] = new Date();
+
+		camp = new Camp();
+		camp.setCreatedBy(staff);
+		camp.setCampName("NTU Camp");
+		camp.setDates(dates);
+		camp.setClosingDate(new Date());
+		camp.setTotalSlots(15);
+		camp.setCommitteeSlot(10);
+		camp.setUserGroup(GroupName.NTU);
+		camp.setDescription("Ur mother's camp");
 	}
 }
 
