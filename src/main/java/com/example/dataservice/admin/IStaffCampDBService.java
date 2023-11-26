7e13fff7833b671b695bb934ca7b157295e9d376
Robin DeBank <40622508+ngeeannrobin@users.noreply.github.com>
2023-11-26 22:44:29 +0800

    Add improvement to staff delete camp operator

--- src/main/java/com/example/dataservice/admin/IStaffCampDBService.java
+++ src/main/java/com/example/dataservice/admin/IStaffCampDBService.java
@@ -5,13 +5,14 @@ import com.example.datastore.operator.IDataStoreEditOperation;
 import com.example.datastore.operator.IDataStoreRetrivalOperation;
 import com.example.datastructure.Camp;
 import com.example.datastructure.Staff;
+import com.example.datastructure.Student;
 
 /**
  * Interface for Staff's access to Camp data.
  */
 public interface IStaffCampDBService {
     IDataStoreEditOperation<Camp> DSCreateCamp(Camp camp, IDataStoreEditable<Staff> staffDataStore);
-    IDataStoreEditOperation<Camp> DSDeleteCamp(Camp camp, IDataStoreEditable<Staff> staffDataStore);
+    IDataStoreEditOperation<Camp> DSDeleteCamp(Camp camp, IDataStoreEditable<Staff> staffDataStore, IDataStoreEditable<Student> studentDataStore);
     IDataStoreEditOperation<Camp> DSEditCamp(Camp camp);
     IDataStoreRetrivalOperation<Camp> DSCampRetrival();
 }
